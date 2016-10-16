684d340f369e4cdcd559aa924fdd770f860bf779
Angela <angela.wilson739@gmail.com>
2016-10-16 11:57:31 -0400

    Created budget schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,27 @@
-drop table if exists entries;
-create table entries (
+drop table if exists account;
+create table account  (
    id integer primary key autoincrement,
-   title text not null,
-   'text' text not null
+   name text not null,
+   email text not null
+   
+
 );
+
+drop table if exists budget;
+create table budget (
+   budgetid integer primary key autoincrement,
+   income integer not null,
+   lineitemid integer,
+   accountid integer, 
+   foreign key (lineitemid) references lineitem(lineitemid),
+   foreign key (accountid) references account(id) 
+); 
+drop table if exists lineitem;
+create table lineitem (
+   lineitemid integer primary key autoincrement,
+   budgetid integer,
+   title text not null,
+   amount integer not null,
+   foreign key (budgetid) references budget(budgetid)
+);
+
